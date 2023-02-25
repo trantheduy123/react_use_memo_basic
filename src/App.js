735bc3b925af4545926e9a89864f42f085ec5b4d
The Duy <trantheduy2309@gmail.com>
2023-02-25 11:30:28 +0700

    finish react_usememo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo } from "react";
+
+function expensiveFunction(number) {
+  console.log("bat dau");
+  const start = new Date();
+
+  //doi 3s
+  while (new Date() - start < 3000)
+    console.log("ket thuc", new Date() - start, "ms");
+
+  return number * number;
+}
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const number = useMemo(() => {
+    expensiveFunction(10);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p> count:{number} </p>
+      <p> count:{count} </p>
+      <button onClick={() => setCount(count + 1)}> add</button>
+    </>
   );
 }
 
